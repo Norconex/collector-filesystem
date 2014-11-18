c703223203bfb1536cb9460cf28577a95d2096f1
kalhomoud <khalid.alhomoud@norconex.com>
2014-11-18 13:42:19 -0500

    Corrected the license information

--- norconex-collector-filesystem/src/main/java/com/norconex/collector/fs/pipeline/queue/FileQueueReferenceStage.java
+++ norconex-collector-filesystem/src/main/java/com/norconex/collector/fs/pipeline/queue/FileQueueReferenceStage.java
@@ -1,19 +1,19 @@
-/* Copyright 2014 Norconex Inc.
+/* Copyright 2013-2014 Norconex Inc.
  * 
- * This file is part of Norconex Collector Core.
+ * This file is part of Norconex Filesystem Collector.
  * 
- * Norconex Collector Core is free software: you can redistribute it and/or 
+ * Norconex Filesystem Collector is free software: you can redistribute it and/or 
  * modify it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * Norconex Collector Core is distributed in the hope that it will be useful, 
+ * Norconex Filesystem Collector is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with Norconex Collector Core. If not, 
+ * along with Norconex Filesystem Collector. If not, 
  * see <http://www.gnu.org/licenses/>.
  */
 package com.norconex.collector.fs.pipeline.queue;
